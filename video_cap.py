982fc510a87c214fbfda3516778a7b7e46e58d1e
tesyolan <tesfa1993@yahoo.com>
2016-10-28 15:36:52 +0300

    Timeit testing for the forward_all pass.

--- video_cap.py
+++ video_cap.py
@@ -9,12 +9,12 @@ from  math import pow
 from PIL import Image, ImageDraw, ImageFont
 import cv2
 caffe_root = '/home/icog-labs/caffe/'
-os.environ['GLOG_minloglevel'] = '2'
+
 sys.path.insert(0, caffe_root + 'python')
 import caffe
-import timeit
-caffe.set_device(0)
-caffe.set_mode_gpu()
+
+# caffe.set_device(0)
+# caffe.set_mode_gpu()
 
 
 
@@ -182,149 +182,49 @@ def nms_max(boxes, overlapThresh=0.3):
 
 def convert_full_conv():
     # Load the original network and extract the fully connected layers' parameters.
-    net = caffe.Net('/home/icog-labs/crop_images/test_1/deploy.prototxt',
-                    '/home/icog-labs/crop_images/test_1/caffe_alexnet_train_iter_2000.caffemodel',
+    net = caffe.Net('deploy.prototxt',
+                    'alexNet__iter_60000.caffemodel',
                     caffe.TEST)
-    params = ['fc6', 'fc7', 'fc8_tune_face']
+    params = ['fc6', 'fc7', 'fc8_flickr']
     fc_params = {pr: (net.params[pr][0].data, net.params[pr][1].data) for pr in params}
     # Load the fully convolutional network to transplant the parameters.
     net_full_conv = caffe.Net('face_full_conv.prototxt',
-                              '/home/icog-labs/crop_images/test_1/caffe_alexnet_train_iter_2000.caffemodel',
+                              'alexNet__iter_60000.caffemodel',
                               caffe.TEST)
     params_full_conv = ['fc6-conv', 'fc7-conv', 'fc8-conv']
     conv_params = {pr: (net_full_conv.params[pr][0].data, net_full_conv.params[pr][1].data) for pr in params_full_conv}
     for pr, pr_conv in zip(params, params_full_conv):
         conv_params[pr_conv][0].flat = fc_params[pr][0].flat  # flat unrolls the arrays
         conv_params[pr_conv][1][...] = fc_params[pr][1]
-    net_full_conv.save('face_full_conv_alex_net_test.caffemodel')
+    net_full_conv.save('face_full_conv.caffemodel')
 
 
 def face_detection(imgList):
     img_count = 0
-    for imgFile in open(imgList).readlines():
-        scales = []
-        factor = 0.793700526
-        img = Image.open(imgFile.strip())
-        # img2 = cv2.imread(imgFile.strip())
-        min = 0
-        max = 0
 
-        if (img.size[0] > img.size[1]):
-            min = img.size[1]
-            max = img.size[0]
-        else:
-            min = img.size[0]
-            max = img.size[1]
-        delim = 2500 / max
-        if (delim == 1):
-            scales.append(1)
-        elif (delim > 1):
-            scales.append(delim)
+    import numpy as np
+    import cv2
 
-        # scales.append(5)
-        min = min * factor
-        factor_count = 1
-        while (min >= 227):
-            scales.append(pow(factor, factor_count))
-            min = min * factor
-            factor_count += 1
-        total_boxes = []
+    cap = cv2.VideoCapture(0)
 
-        print 'size:', img.size[0], img.size[1]
-        print scales
-        for scale in scales:
-            # resize image
+    while (True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-            scale_img = img.resize((int(img.size[0] * scale), int(img.size[1] * scale)))
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            scale_img.save("tmp.jpg")
-            #	print 'size:', scale_img.size[0], scale_img.size[1]
-            # modify the full_conv prototxt.
-            prototxt = open('face_full_conv.prototxt', 'r')
-            new_line = ""
+        # Display the resulting frame
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-            for i, line in enumerate(prototxt):
-                if i == 5:
-                    new_line += "input_dim: " + str(scale_img.size[1]) + "\n"
-                elif i == 6:
-                    new_line += "input_dim: " + str(scale_img.size[0]) + "\n"
-                else:
-                    new_line += line
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
-            output = open('face_full_conv2.prototxt', 'w')
-            output.write(new_line)
-            output.close()
-            prototxt.close()
-
-            net_full_conv = caffe.Net('face_full_conv2.prototxt',
-                                      'face_full_conv_alex_net_test.caffemodel',
-                                      caffe.TEST)
-
-            # load input and configure preprocessing
-            im = caffe.io.load_image("tmp.jpg")
-
-            transformer = caffe.io.Transformer({'data': net_full_conv.blobs['data'].data.shape})
-            transformer.set_mean('data',
-                                     np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1))
-            transformer.set_transpose('data', (2, 0, 1))
-            transformer.set_channel_swap('data', (2, 1, 0))
-            transformer.set_raw_scale('data', 255.0)
-
-
-            # make classification map by forward and print prediction indices at each location
-
-
-            out = net_full_conv.forward_all(data=np.asarray([transformer.preprocess('data', im)]))
-            # out = net_full_conv.forward_all(data=np.asarray([transformer.preprocess('data', im)]))
-
-            # print(timeit.timeit("net_full_conv.forward_all(data=np.asarray([transformer.preprocess('data', im)]))", setup= "import caffe, numpy as np;\
-            #                                                                                             net_full_conv = caffe.Net('face_full_conv2.prototxt',\
-            #                                                                                             'face_full_conv_alex_net_test.caffemodel',caffe.TEST);\
-            #                                                                                             transformer = caffe.io.Transformer({'data': net_full_conv.blobs['data'].data.shape});\
-            #                                                                                             transformer.set_mean('data',np.load('/home/icog-labs/caffe/' + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1));\
-            #                                                                                             transformer.set_transpose('data', (2, 0, 1));\
-            #                                                                                             transformer.set_channel_swap('data', (2, 1, 0));\
-            #                                                                                             transformer.set_raw_scale('data', 255.0);"
-            #                                                                                             "im = caffe.io.load_image('tmp.jpg')",number=10))
-            # # print out['prob'][0].argmax(axis=0)
-            boxes = generateBoundingBox(out['prob'][0, 1], scale)
-            print("11")
-            # plt.subplot(1, 2, 1)
-            # plt.imshow(transformer.deprocess('data', net_full_conv.blobs['data'].data[0]))
-            # plt.subplot(1, 2, 2)
-            # plt.imshow(out['prob'][0,1])
-            # plt.show()
-            # print boxes
-            if (boxes):
-                total_boxes.extend(boxes)
-
-                # boxes_nms = np.array(total_boxes)
-                # true_boxes = nms(boxes_nms, overlapThresh=0.3)
-                # #display the nmx bounding box in  image.
-                # draw = ImageDraw.Draw(scale_img)
-                # for box in true_boxes:
-                #     draw.rectangle((box[0], box[1], box[2], box[3]) )
-                # scale_img.show()
-
-        # nms
-        boxes_nms = np.array(total_boxes)
-        true_boxes1 = nms_max(boxes_nms, overlapThresh=0.3)
-        true_boxes = nms_average(np.array(true_boxes1), overlapThresh=0.07)
-        # display the nmx bounding box in  image.
-        draw = ImageDraw.Draw(img)
-        print "width:", img.size[0], "height:", img.size[1]
-        for box in true_boxes:
-            print("box " + str(box))
-            draw.rectangle((box[0], box[1], box[2], box[3]), outline=(255, 0, 0))
-            font_path = os.environ.get("FONT_PATH", "/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf")
-            ttFont = ImageFont.truetype(font_path, 20)
-            draw.text((box[0], box[1]), "{0:.2f}".format(box[4]), font=ttFont)
-        img.save("result/" + str(img_count) + ".jpg")
-        img_count += 1
-        img.show()
 
 
 if __name__ == "__main__":
     # convert_full_conv()
-
     face_detection("lfw.txt")
